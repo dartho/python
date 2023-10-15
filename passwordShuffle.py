760f02c29f40f80224c68a1f7d917240a1375e63
dartho <dartho@email.com>
2023-10-15 10:01:21 -0400

    adding password shuffle example

---
+++ passwordShuffle.py
@@ -0,0 +1,23 @@
+passwordIn = input("Enter your password: ")
+altLetters = {
+    "a": "@",
+    "E": "3",
+    "e": "3",
+    "i": "!",
+    "I": "1",
+    "o": "0",
+    "O": "0",
+    "u": "U",
+    "y": "4",
+    "Y": "4"
+}
+
+passwordOut = ""
+for letter in passwordIn:
+    if letter in altLetters:
+        passwordOut += altLetters[letter]
+    else:
+        passwordOut += letter
+
+print("Original password: " + passwordIn)
+print("New password: " + passwordOut)
