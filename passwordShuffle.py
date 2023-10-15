d726269ade058feaa8ff2effdbbf4b7032ce1de2
dartho <dartho@email.com>
2023-10-15 13:10:15 -0400

    Revert "whitespace changes"
    This reverts commit 024496031a77c698df94abaa18eaf177710ab503.

--- passwordShuffle.py
+++ passwordShuffle.py
@@ -1,7 +1,16 @@
 passwordIn = input("Enter your password: ")
-altLetters = {"a": "@", "E": "3", "e": "3", "i": "!",
-              "I": "1", "o": "0", "O": "0", "u": "U",
-              "y": "4", "Y": "4"}
+altLetters = {
+    "a": "@",
+    "E": "3",
+    "e": "3",
+    "i": "!",
+    "I": "1",
+    "o": "0",
+    "O": "0",
+    "u": "U",
+    "y": "4",
+    "Y": "4"
+}
 
 passwordOut = ""
 for letter in passwordIn:
