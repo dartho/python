38940ce66230245b6244cef630ab8edc391e1d00
dartho <dartho@email.com>
2023-10-23 15:18:58 -0400

    adding file

---
+++ intToBin.py
@@ -0,0 +1,15 @@
+def int_to_bin(int_val):
+    bin_str = ""
+    while int_val > 0:
+        bit = int_val % 2
+        bin_str += str(bit)
+        int_val = int_val // 2
+    return reverse_string(bin_str)
+
+
+def reverse_string(str_val):
+    return str_val[::-1]
+
+
+my_int = 23
+print(f"{my_int} = {int_to_bin(my_int)}")
