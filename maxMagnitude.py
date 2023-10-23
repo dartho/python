7044e17e6cc9477a218b107c0abc43390be49b45
dartho <dartho@email.com>
2023-10-23 12:40:27 -0400

    added alt bad solution

--- maxMagnitude.py
+++ maxMagnitude.py
@@ -8,7 +8,26 @@ def max_magnitude(vals):
     return max_value * sign
 
 
-values = [int(input()), int(input()), int(input())]
+def max_mag_alt(val1, val2, val3):
+    aval1 = abs(val1)
+    aval2 = abs(val2)
+    aval3 = abs(val3)
+    val_dict = {aval1: (val1 < 0),
+                aval2: (val2 < 0),
+                aval3: (val3 < 0)}
+    max_val = max(aval1, aval2, aval3)
+    sign = -1 if val_dict[max_val] else 1
+    return max(aval1, aval2, aval3) * sign
 
+
+values = [int(input()), int(input()), int(input())]
 max_mag = max_magnitude(values)
+
+#v1 = int(input())
+#v2 = int(input())
+#v3 = int(input())
+#max_mag = max_mag_alt(v1, v2, v3)
+
 print(max_mag)
+
+
