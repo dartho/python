2e62b68f054f743a6aea3899649a78a77f87ab8a
dartho <dartho@email.com>
2023-10-23 16:45:53 -0400

    another file

--- intToBin.py
+++ intToBin.py
@@ -3,7 +3,7 @@ def int_to_bin(int_val):
     while int_val > 0:
         bit = int_val % 2
         bin_str += str(bit)
-        int_val = int_val // 2
+        int_val //= 2
     return reverse_string(bin_str)
 
 
@@ -11,5 +11,5 @@ def reverse_string(str_val):
     return str_val[::-1]
 
 
-my_int = int(input())
-print(f"{my_int} = {int_to_bin(my_int)}")
+my_int = input("Enter an integer: ")
+print(f"{my_int} = {int_to_bin(abs(int(my_int)))}")
